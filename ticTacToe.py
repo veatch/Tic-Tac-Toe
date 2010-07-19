816e754864e892cbcaf106e930cec7b1971e90d7
veatch <jtveatch@gmail.com>
2010-07-18 22:57:02 -0400

    check_for_win() how has a list param, and is now used by computer to choose moves. Computer will check for winning moves, and make them if available. If not, will check for human about to win, and block. Otherwise will take the center cell if available. Lastly, will simply take the next available cell.
    Also added input validation.

--- ticTacToe.py
+++ ticTacToe.py
@@ -25,61 +25,99 @@ def current_player_marker():
         return computer_marker
 
 
-def mark_cell(cell):
-    cell -= 1 # grid starts at 0, grid display starts at 1
-    cell_list[cell] = current_player_marker()
+def mark_cell(cell, marker):
+    cell_list[cell] = marker
 
-def check_for_win():
-    if cell_list.count(current_player_marker()) >= width_and_height:
+def check_for_win(list_to_check, marker):
+    if list_to_check.count(marker) >= width_and_height:
         for i in range(width_and_height):
-            marker = current_player_marker()
-            if cell_list[i] == marker:
-                is_winner = check_for_vertical_win(i, marker)
+            if list_to_check[i] == marker:
+                is_winner = check_for_vertical_win(list_to_check, i, marker)
                 if is_winner:
                     return True
-            if cell_list[i*width_and_height] == marker:
-                is_winner = check_for_horizontal_win(i*width_and_height, marker)
+            if list_to_check[i*width_and_height] == marker:
+                is_winner = check_for_horizontal_win(list_to_check, i*width_and_height, marker)
                 if is_winner:
                     return True
-            is_winner = check_for_left_diagonal_win(marker)
+            is_winner = check_for_left_diagonal_win(list_to_check, marker)
             if is_winner:
                 return True
-            is_winner = check_for_right_diagonal_win(marker)
+            is_winner = check_for_right_diagonal_win(list_to_check, marker)
             if is_winner:
                 return True
+    if ' ' not in list_to_check:
+        return "draw"
     return None
 
-def check_for_vertical_win(column, marker):
+def check_for_vertical_win(list_to_check, column, marker):
     maybe_winner = True
     for j in range (1, width_and_height):
-        if cell_list[column+(j*width_and_height)] != marker:
+        if list_to_check[column+(j*width_and_height)] != marker:
             maybe_winner = False
             break
     return maybe_winner
 
-def check_for_horizontal_win(row, marker):
+def check_for_horizontal_win(list_to_check, row, marker):
     maybe_winner = True
     for j in range (1, width_and_height):
-        if cell_list[row+j] != marker:
+        if list_to_check[row+j] != marker:
             maybe_winner = False
             break
     return maybe_winner
 
-def check_for_left_diagonal_win(marker):
+def check_for_left_diagonal_win(list_to_check, marker):
     maybe_winner = True
     for i in range (width_and_height):
-        if cell_list[i+(i*width_and_height)] != marker:
+        if list_to_check[i+(i*width_and_height)] != marker:
             maybe_winner = False
             break
     return maybe_winner
 
-def check_for_right_diagonal_win(marker):
+def check_for_right_diagonal_win(list_to_check, marker):
     maybe_winner = True
     for i in range (width_and_height):
-        if cell_list[width_and_height-1+(i*(width_and_height-1))] != marker:
+        if list_to_check[width_and_height-1+(i*(width_and_height-1))] != marker:
             maybe_winner = False
             break
     return maybe_winner
+
+def is_valid_input(cell_to_mark):
+    if 0 < cell_to_mark <= width_and_height**2:
+        if cell_list[cell_to_mark] != ' ':
+            print "That cell is already marked. Try again."
+            return False
+        return True
+    print "That number isn't on the grid. Try again."
+    return False
+
+def computer_move():
+    index = is_near_win(computer_marker)
+    if index >= 0:
+        return index
+    index = is_near_win(human_marker)
+    if index >= 0:
+        return index
+    index = width_and_height/2 # center column
+    index += width_and_height*index # center row
+    if cell_list[index] == ' ':
+        return index
+    return cell_list.index(' ');
+
+def is_near_win(marker): 
+    if cell_list.count(marker) >= width_and_height-1:
+        open_space = 0;
+        for i in range ((width_and_height**2)-1):
+            temp_list = cell_list[:]
+            try:
+                open_space = temp_list.index(' ', open_space)
+                temp_list[open_space] = marker
+                win = check_for_win(temp_list, marker)
+                if win:
+                    return open_space
+            except ValueError:
+                pass
+            open_space += 1
+    return -1
 
 width_and_height = 3
 cell_list = [' '] * width_and_height**2
@@ -89,17 +127,29 @@ human_marker = 'O'
 computer_marker = 'X'
 
 draw_grid()
-cell_to_mark = input('Welcome to tic tac toe, human. Pick your cell: ')
-mark_cell(cell_to_mark)
+#todo update first move:
+cell_to_mark = input('Welcome to tic tac toe, human. Pick your cell: ')-1
+mark_cell(cell_to_mark, current_player_marker())
 
 while winner == None:
     is_human_turn = not is_human_turn
     draw_grid()
-    cell_to_mark = input("Your move, human. Remember, you're O: ")
-    mark_cell(cell_to_mark)
-    winner = check_for_win()
+    if is_human_turn:
+        valid = False
+        while not valid:
+            try:
+                cell_to_mark = int(raw_input("Your move, human. Remember, you're O: "))-1 #grid starts at 0, display starts at 1
+                valid = is_valid_input(cell_to_mark)
+            except ValueError:
+                print "That input is not valid. Try one of the numbers on the grid."
+    else:
+        cell_to_mark = computer_move()
+    mark_cell(cell_to_mark, current_player_marker())
+    winner = check_for_win(cell_list, current_player_marker())
 
-if is_human_turn:
+if winner == "draw":
+    print "It's a draw!"
+elif is_human_turn:
     print "human wins? wtf"
 else:
     print "I WIN"
